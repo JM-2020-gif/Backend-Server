9b2a226858466e426fbfacf96bf5992d5f1fdc5b
unknown <andreagutierrez160983@gmail.com>
2021-01-25 23:58:15 +0000

    add file

---
+++ src/controllers/prueba.controller.js
@@ -0,0 +1,27 @@
+const PruebaCtrl={}
+
+PruebaCtrl.obtener=(req,res)=>{
+    res.send('funcionando desde get')
+}
+
+PruebaCtrl.crear=async(req,res)=>{
+    const {nombre, apellido, salario}=req.body
+    const NuevoRegistro=new Empleado({
+        nombre,
+        apellido,
+        salario
+    })
+    await NuevoRegistro.save()
+    res.json({
+        mensaje:'Empleado guardado'
+    })
+}
+
+PruebaCtrl.actualizar=(req,res)=>{
+    res.send('funcionando desde put')
+}
+
+PruebaCtrl.eliminar=(req,res)=>{
+    res.send('funcionando desde delete')
+}
+module.exports=PruebaCtrl
---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express=require('express')
+const app=express()
+const morgan=require('morgan')
+const cors=require('cors')
+const bodyparser=require('body-parser')
+require('./database')
+
+app.set('Port',4000)
+
+app.use(morgan('dev'))
+
+//app.use(bodyparser.urlencoded({extended:true}))
+//app.use(bodyparser.json())
+
+var cors = require('cors')
+
+app.use(cors()) // Use this after the variable declaration
+
+//Rutas
+app.use('/api/',require('./routes/prueba.route'))
+//start server
+
+app.listen(app.get('Port'),()=>{
+    console.log('escuchando por el puerto', app.get('Port'))
+})
